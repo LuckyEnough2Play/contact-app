bd35fae07bf30756b56fa514c0d298ca155f97a1
LuckyEnough2Play <withanyluck@outlook.com>
2025-09-07 10:08:02 -0400

    fix: Update package name and version for bubblecontacts app

--- android/app/src/main/java/com/marbleminds/bubblecontacts/MainActivity.kt
+++ android/app/src/main/java/com/marbleminds/bubblecontacts/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.luckyenough2play.contactapp
+package com.marbleminds.bubblecontacts
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/marbleminds/bubblecontacts/MainApplication.kt
+++ android/app/src/main/java/com/marbleminds/bubblecontacts/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.luckyenough2play.contactapp
+package com.marbleminds.bubblecontacts
 
 import android.app.Application
 import android.content.res.Configuration
